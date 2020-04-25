2fb93899e5231f61205e51156decc518a609142c
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-04-25 17:27:43 +0530

    Exploring Maps in Golang

---
+++ maps.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	leagueTitle := map[string]int{
+		"Kolkata Night Riders": 12,
+		"Mumbai Champions":     10,
+		"Chennai Super Kings":  13,
+	}
+	fmt.Println(leagueTitle)
+}
