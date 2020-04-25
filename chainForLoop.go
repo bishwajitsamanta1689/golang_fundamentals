d93ccad4db42aa97e78359c94813b56432a4d989
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-04-25 15:42:58 +0530

    Nested For Loops in Golang

---
+++ chainForLoop.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// Using Multiple For Loops one inside another
+// Assign 1 variable with a list of items
+// Assign another variable with another list of items
+// Match if any of the items are similar
+func main() {
+	courseInProgress := []string{"Docker Deep Dive", "Go Fundamentals", "Docker and Kubernetes"}
+	courseCompleted := []string{"Go Fundamentals", "Docker Basic", "C++ Beginners"}
+
+	for _, i := range courseInProgress {
+		for _, j := range courseCompleted {
+			if i == j {
+				fmt.Println("Similar Course Found::", j)
+			}
+		}
+	}
+}
