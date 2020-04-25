6bd18390240533658055bd0e63c27bded6f905d9
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-04-25 12:29:36 +0530

    Added Algorithm for Variadic Functions.

--- variadic_func.go
+++ variadic_func.go
@@ -2,7 +2,11 @@ package main
 
 import "fmt"
 
-//Take any number of inputs and Returns the lowest
+// Variadic Functions
+// A Variable having a Function with n numbers of arguments.
+// Create a Function with ellipses which can take n number of arguments.
+// Assign the first instance of the argument to a variable
+// Find across the range of the Variable and assign the lowest value and return it back.
 
 func main() {
 	bestFinish := bestFinishLeague(2, 15, 12, 13, 16, 18, 5, 1)
