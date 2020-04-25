ecc8e88047597255ea132a88f4d486c9e51d3c32
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-04-25 15:52:56 +0530

    Added Break Labels in Nested For Loop

--- chainForLoop.go
+++ chainForLoop.go
@@ -10,10 +10,14 @@ func main() {
 	courseInProgress := []string{"Docker Deep Dive", "Go Fundamentals", "Docker and Kubernetes"}
 	courseCompleted := []string{"Go Fundamentals", "Docker Basic", "C++ Beginners"}
 
+	// Break Labels are used to exit multiple for loops break and go to the top of the code.
+
+checkPoint:
 	for _, i := range courseInProgress {
 		for _, j := range courseCompleted {
 			if i == j {
 				fmt.Println("Similar Course Found::", j)
+				break checkPoint
 			}
 		}
 	}
